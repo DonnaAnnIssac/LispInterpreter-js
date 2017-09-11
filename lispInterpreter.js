ca59b719f661a1922e1e173ddb338acced2e1f70
Donna Ann Issac <donna.ann1209@gmail.com>
2017-09-11 12:02:41 +0530

    Refactor code

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -17,26 +17,28 @@ function programParser(input) {
   return
 }
 const defaultEnv = {
-  '+' : add = function(args) { return (result = args.reduce(function(sum, value) { return parseFloat(sum + value) }))},
-  '-' : sub = function(args) { return (result = args.reduce(function(diff, value) { return parseFloat(diff - value) }))},
-  '*' : mul = function(args) { return (result = args.reduce(function(prod, value) { return parseFloat(prod * value) }))},
-  '/' : div = function(args) { return (result = args.reduce(function(quot, value) { return parseFloat(quot / value) }))},
-  '<' : lesser = function(args) { return (args[0] < args[1] ? true : false)},
-  '>' : greater = function(args) { return (args[0] > args[1] ? true : false)},
-  '=' : equals = function(args) { return (args[0] === args[1] ? true : false)},
-  '<=' : lesserOrEqual = function(args) { return (args[0] <= args[1] ? true : false)},
-  '>=' : greaterOrEqual = function(args) { return (args[0] >= args[1] ? true : false)},
-  'list' : lists = function(args) { return args},
-  'car' : car = function(args) { return args[0][0]},
-  'cdr' : cdr = function(args) { return args[0].slice(1)},
-  'cons' : cons = function(args) { args[1].push(args[0])
-                                   return args[1]},
-  'print' : print = function(args) { console.log(args.join(' ')) }
+  '+' : add = (args) => args.reduce((sum, value) => parseFloat(sum + value)),
+  '-' : sub = (args) => args.reduce((diff, value) => parseFloat(diff - value)),
+  '*' : mul = (args) => args.reduce((prod, value) => parseFloat(prod * value)),
+  '/' : div = (args) => args.reduce((quot, value) => parseFloat(quot / value)),
+  '<' : lesser = (args) => args[0] < args[1] ? true : false,
+  '>' : greater = (args) => args[0] > args[1] ? true : false,
+  '=' : equals = (args) => args[0] === args[1] ? true : false,
+  '<=' : lesserOrEqual = (args) => args[0] <= args[1] ? true : false,
+  '>=' : greaterOrEqual = (args) => args[0] >= args[1] ? true : false,
+  'list' : lists = (args) => args,
+  'car' : car = (args) => args[0][0],
+  'cdr' : cdr = (args) => args[0].slice(1),
+  'cons' : cons = (args) => { args[1].push(args[0])
+                            return args[1]},
+  'print' : print = function(args) { console.log(args.join(' ')) },
+  'max' : max = (args) => args.reduce((max, curr) => (max > curr) ? max : curr),
+  'min' : min = (args) => args.reduce((min, curr) => (min < curr) ? min : curr)
 }
 
 var globalEnv = {}
 
-const s_expressionParser = function (input) {
+const s_expressionParser = (input) => {
   if(input[0] !== '(') return null
   input = (spaceParsedData = spaceParser(input.slice(1)) !== null) ? spaceParsedData[1] : input.slice(1)
   let resultArray = specialParsers(input), temp
@@ -53,11 +55,10 @@ const s_expressionParser = function (input) {
   if(resultArray.length === 0) return
   if (resultArray.length === 1) return ([resultArray[0], input])
   let result = funcEvaluator(resultArray, defaultEnv)
-  input = (spaceParsedData = spaceParser(input) !== null) ? spaceParsedData[1] : input
-  return ((result !== null) ? ([result, input]) : null)
+  return ((result !== null) ? ([result, input.slice(1)]) : null)
 }
 
-const ifParser = function(input) {
+const ifParser = (input) => {
   if(input.substr(0, 2) !== 'if') return null
   let condition = expressionParser(input.slice(3))
   input = ((spaceParsedData = spaceParser(condition[1])) !== null) ? spaceParsedData[1] : condition[1]
@@ -69,25 +70,25 @@ const ifParser = function(input) {
   }
 }
 
-const defParser = function(input) {
+const defParser = (input) => {
   if(input.substr(0, 6) !== 'define') return null
   let identifier = symbolParser(input.slice(7)), result
   if(identifier === null) return ([null, input])
   input = ((spaceParsedData = spaceParser(identifier[1])) !== null) ? spaceParsedData[1] : identifier[1]
   if((result = lambdaParser(input.slice(1))) !== null) {
     globalEnv[identifier[0]] = result[0]
-    result = ((spaceParsedData = spaceParser(result[1]))!== null) ? spaceParsedData[1] : result[1]
+    result = ((spaceParsedData = spaceParser(result[1]))!== null) ? spaceParsedData[1] : result[1].slice(1)
     return ([ , result])
   }
   let value = expressionParser(input)
   if (value !== null) {
     globalEnv[identifier[0]] = value[0]
-    value = ((spaceParsedData = spaceParser(value[1].slice(1))) !== null) ? spaceParsedData[1] : value[1].slice(1)
+    value = ((spaceParsedData = spaceParser(value[1].slice(1))) !== null) ? spaceParsedData[1] : value[1].slice(2)
     return([ ,value])}
   return ([null, input])
 }
 
-const lambdaParser = function(input) {
+const lambdaParser = (input) => {
   if(input.substr(0, 6) !== 'lambda') return null
   let formals = [], fnObj = {}
   input = input.slice(8)
@@ -104,8 +105,7 @@ const lambdaParser = function(input) {
   input = ((spaceParsedData = spaceParser(input.slice(funcBody.length + 1))) !== null) ? spaceParsedData[1] : input.slice(funcBody.length + 1)
   if(input[0] !== ')' && input[0] !== '(') {
     let params = []
-    while(input[0] !== ')' && input[0] !== '(') //if parameters passed directly to lambda
-    {
+    while(input[0] !== ')' && input[0] !== '(') {
       let temp = expressionParser(((spaceParsedData = spaceParser(input)) !== null) ? spaceParsedData[1] : input)
       params.push(temp[0])
       input = temp[1]
@@ -116,20 +116,19 @@ const lambdaParser = function(input) {
   return ([fnObj, input])
 }
 
-const spaceParser = function(input) {return (((/^(\s|\n)+/).test(input)) ? ([' ', input.replace(/^(\s|\n)+/, '')]) : null)}
+const spaceParser = (input) => ((/^(\s|\n)+/).test(input)) ? ([' ', input.replace(/^(\s|\n)+/, '')]) : null
 
-const numParser = function(input) {
+const numParser = (input) => {
   let parsedNum = (/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/).exec(input)
   if(parsedNum) {
-    parsedNum = parsedNum[0]
-    let resData = input.slice(parsedNum.length)
+    let resData = input.slice(parsedNum[0].length)
     parsedNum = parseFloat(parsedNum)
     return ([parsedNum, resData])
   }
   return null
 }
 
-const stringParser = function (input) {
+const stringParser = (input) => {
   if (input[0] !== '"') return null
   let i = input.slice(1).search(/"(\s|\))/)
   let parsedString = input.slice(1,i+1)
@@ -137,7 +136,7 @@ const stringParser = function (input) {
   return (((spaceParsedData = spaceParser(resData)) !== null) ? ([parsedString, spaceParsedData[1]]) : ([parsedString, resData]))
 }
 
-const boolParser = function(input) {
+const boolParser = (input) => {
   if(input[0] === 'T')
     return (((spaceParsedData = spaceParser(input.slice(1))) !== null) ? ([true, spaceParsedData[1]]) : ([true, input.slice(1)]))
   if(input[0].substr(0, 3) === 'NIL')
@@ -145,7 +144,7 @@ const boolParser = function(input) {
   return null
 }
 
-const symbolParser = function (input) {
+const symbolParser = (input) => {
   let parsedName = (/^[a-zA-Z'=+\-*\/\<\>]+/).exec(input)
   if(parsedName) {
     parsedName = parsedName[0]
@@ -157,26 +156,26 @@ const symbolParser = function (input) {
 
 function anyParserFactory(...parsers) {
   return function(input) {
-    for(let i = 0; i < parsers.length; i++) {
-      let result = parsers[i](input)
+    for(parser of parsers) {
+      let result = parser(input)
       if(result !== null)return result
     }
-  return null
+    return null
   }
 }
 
 const expressionParser = anyParserFactory(boolParser, numParser, stringParser, symbolParser, s_expressionParser)
+
 const specialParsers = anyParserFactory(ifParser, defParser, lambdaParser)
 
 function funcEvaluator(input, env) {
   if (input.length === 0) return 'NIL'
   if (input.length === 1) return null
-  let func = input[0], flag = 0, args = input.slice(1)
-  for(let i = 0; i < args.length; i++) {
-    if(symbolParser(args[i]) !== null && globalEnv[args[i]] !== undefined)
-      args[i] = globalEnv[args[i]]
+  let func = input[0], args = input.slice(1)
+  for(let arg of args) {
+    if(symbolParser(arg) !== null && globalEnv[arg] !== undefined)
+      arg = globalEnv[arg]
   }
-
   return (env.hasOwnProperty(func) ? env[func](args) : procEvaluator(func, args))
 }
 
@@ -184,12 +183,10 @@ function procEvaluator(func, args) {
   func = globalEnv.hasOwnProperty(func) ? globalEnv[func] : func
   if(func.args.length !== args.length) return null
   let arr = func.args, str = func.body
-  for(let i = 0; i < args.length; i++)
+  for(let i = 0; i < args.length; i+)
     func.env[arr[i]] = args[i]
-
-  for (let argName of arr) {
+  for (let argName of arr)
       str = str.replace(new RegExp(argName, 'g'), func.env[argName])
-  }
   let result = s_expressionParser(str)
   return result[0]
 }
